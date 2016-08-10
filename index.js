24b72c41f62fe1ac5bdbd7bd387d53aa6a2f1100
Andrew54757 <andrews54757@gmail.com>
2016-08-10 14:11:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,16 +28,23 @@ var subtraction = init(settings.subtractions.percent)
 var multiplication = init(settings.multiplication.percent)
 var division = init(settings.division.percent)
 var worksheet = "                           M A T H   W O R K S H E E T                       /n";
+var answer = "                              A N S W E R S                           /n";
+var numbers = []
+function push(a,b,op) {
+  numbers.push({a:a,b:b,op:op})
+  
+}
 for (var i = 0; i < settings.amount; i++) {
   var ra = Math.floor(Math.random() * percentsum) + 1
   if (addition.check(ra)) {
-    // addition
-    var numbers = [Math.floor(Math.random()*settings.adsition.max) + min,Math.floor(Math.random()*settings.adsition.max) + min]
-    
-    
+    // addition (0)
+    var numbe = [Math.floor(Math.random()*settings.addition.max) + settings.addition.min,Math.floor(Math.random()*settings.addition.max) + settings.addition.min]
+      push(numbe[0],numbe[1],0)
+   
     
   } else if (subtraction.check(ra)) {
-    
+    var sett = settings.subtraction
+    var numbe = [Math.floor(Math.random()*sett.max)+sett.min,Math.floor(Math.random()*sett.max)+sett.min]
     
   } else if (multiplication.check(ra)) {
     
