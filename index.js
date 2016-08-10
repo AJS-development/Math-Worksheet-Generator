4a4ea1a565149f9dbe848160361f9a04eeebdf74
Andrew54757 <andrews54757@gmail.com>
2016-08-10 13:37:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+"use strict";
+const fs = require('fs')
 
+try {
+const settings = JSON.parse(fs.readFileSync(__dirname + "/settings.json","utf8"))
+console.log("Loaded settings")
+} catch (e) {
+  console.log("failed to load settings, " + e);
+  process.exit()
+}
+var percentsum = 0;
+function init(a) {
+  if (!a) return {
+   min: -1000,
+   max: -1000
+  }
+  percentsum = a + percentsum;
+  return {
+   min: percentsum - a, 
+    max: percentsum
+  }
+}
+var addition = init(settings.addition)
+var subtraction = init(settings.subtractions)
+
+
+for (var i = 0; i < settings.amount; i++) {
+  
+  
+  
+  
+  
+}
